e81ce0f4a6279a244358bbd11860f845383f554a
ShreehariVaasishta <shreehari9481@gmail.com>
2023-07-01 20:50:56 +0530

    Added reading of config json for each phases and execute them in respective phases.

--- runner/main.go
+++ runner/main.go
@@ -1,49 +1,129 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os/exec"
 )
 
-type PhaseConfig struct {
-	Name   string
-	Script string
+// Phases configs
+type PhasesConfig struct {
+	BuildPhase
+	ArtifactsPhase
+	FinalizePhase
 }
 
-func executePhase(phase PhaseConfig) error {
-	fmt.Printf("Executing phase: %s\n", phase.Name)
-	cmd := exec.Command("sh", "-c", phase.Script)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("failed to execute phase %s: %s, error: %v", phase.Name, string(output), err)
-	}
-	return nil
+type BuildPhase struct {
+	Build []string `json:"build"`
 }
 
-func handlePhases(phases []PhaseConfig) error {
-	for _, phase := range phases {
-		err := executePhase(phase)
+type ArtifacsNestedPhase struct {
+	LocalTargetDir  string `json:"local_target_dir"`
+	RemoteTargetDir string `json:"remote_target_dir"`
+}
+type ArtifactsPhase struct {
+	Artifacts ArtifacsNestedPhase `json:"artifacts"`
+}
+
+type FinalizePhase struct {
+	Finalize []string `json:"finalize"`
+}
+
+// Pod Specific Configs
+type PodConfig struct {
+	CPU    string `json:"cpu"`
+	Memory string `json:"memory"`
+}
+
+type JsonConfigStruct struct {
+	PodCfg PodConfig    `json:"pod"`
+	Phases PhasesConfig `json:"phases"`
+}
+
+func executeBuildPhase(buildCommands []string) error {
+	log.Println("Starting execution in Phase: Build")
+	for _, cmd := range buildCommands {
+		log.Println("Execution Command: ", cmd)
+		_cmd := exec.Command("sh", "-c", cmd)
+		tOutput, err := _cmd.CombinedOutput()
 		if err != nil {
 			return err
 		}
+		log.Println(tOutput)
+	}
+	return nil
+
+}
+
+func executeArtifactsPhase(local_target_dir string, remote_target_dir string) error {
+	log.Println("Starting execution in Phase: Upload Artifacts")
+	log.Println("Uploading files in ", local_target_dir, "to ", remote_target_dir)
+	return nil
+}
+
+func executeFinalizePhase(finalizeCommands []string) error {
+	log.Println("Starting execution in Phase: Finalize")
+	for _, cmd := range finalizeCommands {
+		log.Println("Execution Command: ", cmd)
+		_cmd := exec.Command("sh", "-c", cmd)
+		tOutput, err := _cmd.CombinedOutput()
+		if err != nil {
+			return err
+		}
+		log.Println(tOutput)
+	}
+	return nil
+
+}
+
+func handlePhases(phases PhasesConfig) error {
+
+	// Build Phase
+	err := executeBuildPhase(phases.Build)
+	if err != nil {
+		return err
+	}
+
+	// Artifacs Phase
+	err = executeArtifactsPhase(phases.Artifacts.LocalTargetDir, phases.Artifacts.RemoteTargetDir)
+	if err != nil {
+		return err
+	}
+
+	// Finalize Phase
+	err = executeFinalizePhase(phases.Finalize)
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
-func main() {
-	// Define the phases
-	phases := []PhaseConfig{
-		{Name: "Download Source", Script: "echo 'Downloading source'"},
-		{Name: "Environment", Script: "echo 'Setting up environment'"},
-		{Name: "Install", Script: "echo 'Installing dependencies'"},
-		{Name: "Build", Script: "echo 'Building the project'"},
-		{Name: "Upload artifacts", Script: "echo 'Uploading artifacts'"},
-		{Name: "Finalize", Script: "echo 'Finalizing the process'"},
+func readConfigJson(filePath string) (PhasesConfig, error) {
+	// Read the file
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Println(err)
+		return PhasesConfig{}, err
 	}
+	var jsonconfig PhasesConfig
+	err = json.Unmarshal(data, &jsonconfig)
 
+	if err != nil {
+		fmt.Println(err)
+		return PhasesConfig{}, err
+	}
+	return jsonconfig, nil
+
+}
+
+func main() {
+	configJson, err := readConfigJson("/config/config.json")
+
+	fmt.Println(configJson)
 	// Execute the phases
-	err := handlePhases(phases)
+	err = handlePhases(configJson)
 	if err != nil {
 		log.Fatalf("Error executing phases: %v", err)
 	}
